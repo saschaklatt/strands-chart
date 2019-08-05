9fd9510d31f984b0bf16f85c14fc4b15743afeaa
Sascha Klatt <coding@klattiation.de>
2019-08-05 15:31:33 +0200

    use curved bezier path for strand

--- src/strands-chart/index.js
+++ src/strands-chart/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./StrandsChart.css"
 
 const EXAMPLE_SEQUENCE = [
+  0,
   1,
   1,
   2,
@@ -27,6 +28,7 @@ const EXAMPLE_SEQUENCE = [
   2,
   4,
   5,
+  0,
 ]
 
 const scaleLinear = (value, domain, range) => {
@@ -54,7 +56,7 @@ const getAlignedX = (x, width, direction) => {
   }
 }
 
-const getPoints = (sequence, width, height, direction) => {
+const getTupels = (sequence, width, height, direction) => {
   const domainY = [0, sequence.length - 1]
   const rangeY = [height, 0]
 
@@ -64,7 +66,7 @@ const getPoints = (sequence, width, height, direction) => {
   const scaleX = makeLinearScaler(domainX, rangeX)
   const scaleY = makeLinearScaler(domainY, rangeY)
 
-  const tupels = sequence.map((v, i) => {
+  return sequence.map((v, i) => {
     const x = scaleX(v)
     const y = scaleY(i)
     const alignedX = getAlignedX(x, width, direction)
@@ -72,19 +74,80 @@ const getPoints = (sequence, width, height, direction) => {
     const p2 = [alignedX[1], y]
     return [p1, p2]
   })
+}
 
-  const leftPoints = tupels.map(tupel => tupel[0])
-  const rightPoints = tupels.map(tupel => tupel[1])
-  const shape = [...leftPoints, ...rightPoints.reverse()]
+const tupelsToOutlineLeft = tupels => tupels.map(tupel => tupel[0])
+const tupelsToOutlineRight = tupels => tupels.map(tupel => tupel[1]).reverse()
 
-  return shape.map(p => `${p[0]},${p[1]}`).join(" ")
+const tupelsToOutline = tupels => [
+  ...tupelsToOutlineLeft(tupels),
+  ...tupelsToOutlineRight(tupels),
+]
+
+const getPoints = (sequence, width, height, direction) => {
+  const tupels = getTupels(sequence, width, height, direction)
+  const outline = tupelsToOutline(tupels)
+  return outline.map(p => `${p[0]},${p[1]}`).join(" ")
+}
+
+const makeQuadBezierPath = points => {
+  const copy = [...points]
+  const p0 = copy.shift()
+  const p1 = copy.shift()
+  const c = [(p0[0] + p1[0]) / 2, (p0[1] + p1[1]) / 2]
+  const q = [p0[0], c[1]]
+  const pathStart = `M ${p0[0]} ${p0[1]} Q ${q[0]} ${q[1]}, ${p1[0]} ${p1[1]}`
+  const path = copy.map(p => `T ${p[0]} ${p[1]}`).join(" ")
+
+  return `${pathStart} ${path}`
+}
+
+const getCubicC0 = (p0, p1, curving) => [
+  p0[0],
+  p0[1] + (p1[1] - p0[1]) * curving,
+]
+
+const getCubicC1 = (p0, p1, curving) => [
+  p1[0],
+  p1[1] - (p1[1] - p0[1]) * curving,
+]
+
+const pointToString = p => `${p[0]} ${p[1]}`
+
+const makeCubicBezierPath = (points, curving = 1) =>
+  points.reduce((path, p, idx, arr) => {
+    if (idx === 0) {
+      return path
+    }
+    const p0 = arr[idx - 1]
+    const P1 = pointToString(p)
+    const C0 = pointToString(getCubicC0(p0, p, curving))
+    const C1 = pointToString(getCubicC1(p0, p, curving))
+    return `${path} C ${C0} ${C1} ${P1}`
+  }, "")
+
+const getPath = (sequence, width, height, direction) => {
+  const tupels = getTupels(sequence, width, height, direction)
+  const outlineLeft = tupelsToOutlineLeft(tupels)
+  const outlineRight = tupelsToOutlineRight(tupels)
+
+  const curving = 0.5
+  const p0Left = pointToString(outlineLeft[0])
+  const p0Right = pointToString(outlineRight[0])
+  const pathStart = `M${p0Left}`
+  const pathOutlineLeft = makeCubicBezierPath(outlineLeft, curving)
+  const pathEdge = `L${p0Right}`
+  const pathOutlineRight = makeCubicBezierPath(outlineRight, curving)
+  const pathEnd = "Z"
+
+  return `${pathStart} ${pathOutlineLeft} ${pathEdge} ${pathOutlineRight} ${pathEnd}`
 }
 
 const StrandsChart = ({ width, height }) => (
   <svg className="strands-chart" width={width} height={height}>
-    <polygon
+    <path
       className="strands-chart--strand"
-      points={getPoints(EXAMPLE_SEQUENCE, 100, height, "center")}
+      d={getPath(EXAMPLE_SEQUENCE, 100, height, "center")}
     />
   </svg>
 )
